8f517594a47bcc16f34a494e5dffbb380d4f7b2a
ananya24-glitch <kumariananya096@gmail.com>
2025-09-07 11:30:10 +0530

    php css

--- index.php
+++ index.php
@@ -22,7 +22,7 @@ $techresult = $conn->query($tech);
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/7.0.0/css/all.min.css"
         integrity="sha512-DxV+EoADOkOygM4IR9yXP8Sb2qwgidEmeqAEmDKIOfPRQZOWbXCzLC6vjbZyy0vPisbH2SyW27+ddLVCN+OMzQ=="
         crossorigin="anonymous" referrerpolicy="no-referrer" />
-    <link rel="stylesheet" href="nav.css">
+    <link rel="stylesheet" href="index.css">
     <title>Document</title>
 </head>
 
